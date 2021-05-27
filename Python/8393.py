d0b92440b4105f251df7585fb167f407a1b6043d
Jaehan Lee <leejh17025@gmail.com>
2021-05-27 15:19:54 +0900

    2문제 풀이

--- Python/8393.py
+++ Python/8393.py
@@ -1,5 +1,8 @@
-n = int(input())
-a = 0
-for i in range(1, n + 1):
-    a += i
-print(a)
+# n = int(input())
+# a = 0
+# for i in range(1, n + 1):
+#     a += i
+# print(a)
+
+a = int(input())
+print(int(a*(a+1)/2))
